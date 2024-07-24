e9b100035bf75c2e36ce89ac19e4bc7ac80d5e0c
tuaandatt <146009768+tuaandatt@users.noreply.github.com>
2024-07-24 13:40:17 +0700

    init project

---
+++ timkiemnhiphan_khongdequi/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Lam Tuan Dat
+//2033212004
+//buoi 3
+internal class TimKiemNhiPhan_KhongDeQui
+{
+    static void Main()
+    {
+        int[] a;
+        int n, x;
+
+        do
+        {
+            Console.Write("Nhap so phan tu cua mang: ");
+            n = int.Parse(Console.ReadLine());
+        } while (n <= 0);
+
+        a = new int[n];
+
+        nhapMang(a, n);
+        xuatMang(a, n);
+
+        Console.Write("\nNhap x can tim: ");
+        x = int.Parse(Console.ReadLine());
+
+        int kq = binarySearch(a, n, x);
+
+        if (kq == -1)
+            Console.WriteLine($"\nKhong tim thay {x} trong mang");
+        else
+            Console.WriteLine($"\nTim thay {x} tai vi tri thu {kq}");
+    }
+
+    static void nhapMang(int[] a, int n)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rand.Next(1, 100);
+        }
+    }
+
+    static void xuatMang(int[] a, int n)
+    {
+        Console.WriteLine();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"\t{a[i]}");
+        }
+        Console.WriteLine();
+    }
+
+    static int binarySearch(int[] a, int n, int key)
+    {
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            if (a[mid] == key)
+                return mid;
+
+            if (key < a[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return -1;
+    }
+}
---
+++ tinhtong_khongdequi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Lam Tuan Dat
+//2033212004
+//buoi 3
+internal class TinhTong_KhongDeQui
+{
+    static void NhapMang(out int[] a, out int n)
+    {
+        Random rand = new Random();
+        do
+        {
+            Console.Write("Nhap so phan tu cua mang: ");
+            n = int.Parse(Console.ReadLine());
+        } while (n <= 0);
+
+        a = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rand.Next(1, 100);
+        }
+    }
+
+    static void XuatMang(int[] a, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"\t{a[i]}");
+        }
+        Console.WriteLine();
+    }
+
+    static int Sum(int[] a, int l, int r)
+    {
+        int sum = 0;
+        for (int i = l; i <= r; i++)
+        {
+            sum += a[i];
+        }
+        return sum;
+    }
+
+    static void Main()
+    {
+        NhapMang(out int[] a, out int n);
+        Console.WriteLine("Mang da nhap:");
+        XuatMang(a, n);
+
+        int ketqua = Sum(a, 0, n - 1);
+        Console.WriteLine($"\nKet qua (khong de qui): {ketqua}");
+    }
+}
